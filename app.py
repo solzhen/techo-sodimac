5c4ebf0ab04eef40c022a4390d7c26caca563810
jpintoriv <jorgepintoriveros@gmail.com>
2017-12-22 12:06:50 -0300

    add port socket

--- app.py
+++ app.py
@@ -182,4 +182,6 @@ class Sodimac(object):
 
 
 if __name__ == '__main__':
+  cherrypy.config.update({'server.socket_host': '0.0.0.0',})
+  cherrypy.config.update({'server.socket_port': int(os.environ.get('PORT', '5000')),})
   cherrypy.quickstart(Sodimac())
